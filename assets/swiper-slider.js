03ca3e348cbc5df1a98fcc11bbfbf115b934e769
ILYA <62027029+ilya9488@users.noreply.github.com>
2024-06-14 17:59:00 +0300

    edits: product slider (centered)

--- assets/swiper-slider.js
+++ assets/swiper-slider.js
@@ -7,15 +7,24 @@ function swiperSliderInit() {
   const sliders = document.querySelectorAll('.swiper-container')
 
   sliders.forEach(slider => {
-    const columns = slider.dataset.columns ?? 3
-    const centered = slider.dataset.centered === 'align_middle'
+    const columns = slider.dataset.columns ?? 3;
+    const centered = slider.dataset.centered === 'align_middle';
+    const slidesCount = slider.querySelector('.swiper-wrapper').children.length;
+
+    if (centered && slidesCount < columns) {
+      slider.querySelector('.swiper-wrapper').classList.add('align_middle');
+    }
+
+    const mediaQuery = window.matchMedia('(max-width: 425px)');
+    if (mediaQuery.matches) {
+      slider.querySelector('.swiper-wrapper').classList.remove('align_middle');
+    }
     
       new Swiper(slider, {
         slidesPerView: columns,
         spaceBetween: 0,
         slidesPerColumn: 1,
         watchOverflow: true,
-        centeredSlides: centered,
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
@@ -23,23 +32,23 @@ function swiperSliderInit() {
         pagination: {
           el: ".swiper-pagination",
         },
-          breakpoints: {
-        // when window width is >= 320px
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        // when window width is >= 480px
-        425: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        },
-        // when window width is >= 640px
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 10
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          // when window width is >= 480px
+          425: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          // when window width is >= 640px
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 10
+          }
         }
-        },
       });
   })
 }
