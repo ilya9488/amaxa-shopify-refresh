73ab8f2e6a8317180d633fe1d017691c085f8075
ILYA <62027029+ilya9488@users.noreply.github.com>
2024-06-17 16:06:27 +0300

    responsive: Component Sleder

--- assets/swiper-slider.js
+++ assets/swiper-slider.js
@@ -14,11 +14,19 @@ function swiperSliderInit() {
     if (centered && slidesCount < columns) {
       slider.querySelector('.swiper-wrapper').classList.add('align_middle');
     }
-
-    const mediaQuery = window.matchMedia('(max-width: 425px)');
-    if (mediaQuery.matches) {
-      slider.querySelector('.swiper-wrapper').classList.remove('align_middle');
+    
+    function swiperHandleResize() {
+      const mediaQuery = window.matchMedia('(max-width: 600px)');
+      if (mediaQuery.matches) {
+        slider.querySelector('.swiper-wrapper').classList.remove('align_middle');
+      } else {
+        if (centered && slidesCount < columns) {
+          slider.querySelector('.swiper-wrapper').classList.add('align_middle');
+        }
+      }
     }
+    swiperHandleResize();
+    window.addEventListener('resize', swiperHandleResize);
     
       new Swiper(slider, {
         slidesPerView: columns,
@@ -33,18 +41,23 @@ function swiperSliderInit() {
           el: ".swiper-pagination",
         },
         breakpoints: {
-          // when window width is >= 320px
-          320: {
+          // when window width is >= 0px
+          0: {
             slidesPerView: 1,
             spaceBetween: 10
           },
-          // when window width is >= 480px
-          425: {
+          // when window width is >= 600px
+          600: {
             slidesPerView: 2,
             spaceBetween: 10
           },
-          // when window width is >= 640px
-          768: {
+          // when window width is >= 990px
+          990: {
+            slidesPerView: 3,
+            spaceBetween: 10
+          },
+          // when window width is >= 1300px
+          1300: {
             slidesPerView: 4,
             spaceBetween: 10
           }
