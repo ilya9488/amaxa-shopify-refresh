563396b0ce38faafffe057c0ef9a904fbeeeba90
ILYA <62027029+ilya9488@users.noreply.github.com>
2024-06-20 15:40:03 +0300

    add:  smooth scroll for anchor

--- assets/custom.js
+++ assets/custom.js
@@ -7,7 +7,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
     if (targetElement) {
       window.scrollTo({
-        top: targetElement.offsetTop,
+        top: targetElement.offsetTop - 100,
         behavior: 'smooth'
       });
     }
